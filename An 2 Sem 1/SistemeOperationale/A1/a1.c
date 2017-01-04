366b7cb923f2c6295a9157a375ca65942d27a83c
Cretu Calin <cretucalin06@yahoo.ro>
2017-01-04 18:56:59 +0200

    A1 So

--- An 2 Sem 1/SistemeOperationale/A1/a1.c
+++ An 2 Sem 1/SistemeOperationale/A1/a1.c
@@ -3,17 +3,22 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 void appendFile(int fd[],int myargc, char c)
 {
-	if(write(fd[myargc],c , 2) < 2)
-		printf("Error writing in file\n");
+	if(write(fd[myargc], &c , 1) < 1)
+	{
+		perror("Error writing char in file");
+	}
 }
 
 void appendFileInt(int fd[],int myargc, int c)
 {
-	if(write(fd[myargc],c, 4) < 4)
-		printf("Error writing in file\n");
+	if(write(fd[myargc],&c, 4) < 4)
+	{
+		perror("Error writing int in file");
+	}
 }
 
 void readFile(int fds[],int myargc, int fd)
@@ -25,8 +30,8 @@ void readFile(int fds[],int myargc, int fd)
         char t = 0;
         bytes_read = read(fd, &t, 1); 
         buffer[k++] = t; 
-		appendFile(fds,myargc,t); 
         printf("%c", t);
+		appendFile(fds,myargc,t); 
         if(t == '\n' && t == '\0') {
             printf("%d", atoi(buffer));
 			appendFileInt(fds,myargc,atoi(buffer));
@@ -39,33 +44,48 @@ void readFile(int fds[],int myargc, int fd)
 }
 
 
-int myargc;
-int fd[50];
+int fd[50]={0};
+
 int main(int argc, char *argv[])
 {
 	int i;
 
 	if(argc >= 3)
 	{
-	myargc = argc;
-	fd[argc]= open (argv[i], O_CREAT|O_APPEND|O_WRONLY, 0600);
+	argc = argc;
+	fd[argc-1]= open (argv[argc-1], O_CREAT|O_APPEND|O_WRONLY, 0700);
 	if (fd[argc] < 0)
-		printf("Error creating file for writing\n ");
+	{
+		perror("Error creating file for writing");
+	}
+		
 
 	for(i =1; i<=argc-2; i++)
 	{
-		fd[i] = open (argv[i],O_APPEND|O_RDONLY , 0600);
+		printf("\n%d %s",i, argv[i]);
+	
+
+		fd[i] = open (argv[i], O_RDONLY);
 		
 		if (fd[i] < 0)
-			printf("Error creating read file descriptor\n");
-		else{
-			readFile(fd,myargc, fd[i]);
-			close(fd[i]);
+		{
+			perror("Error creating read file descriptor");
+		}
+		else
+		{
+			readFile(fd,argc-1, fd[i]);
+			printf("readFile2)");
+			if(close(fd[i]) < 0)
+			{
+			perror("Close file for reading");
+			}
+			
 		}
 		
 	}
 
-	close(fd[argc]);
+	if(close(fd[argc])<0)
+	perror("Close file for writing");
 
 	}
 	else
